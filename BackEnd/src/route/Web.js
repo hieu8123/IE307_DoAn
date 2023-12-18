2a3e9e1e90833a2abe29af01f59faeae37115fac
2150372 <21520372@gm.uit.edu.vn>
2023-12-18 23:38:58 +0700

    update route

--- BackEnd/src/route/Web.js
+++ BackEnd/src/route/Web.js
@@ -20,21 +20,6 @@ const initWebRoutes = (app) => {
     app.get('/wishlist', [AuthController.authenticateUser], UserController.getAllWishListByUser);
     app.post('/wishlist', [AuthController.authenticateUser], UserController.addWishList);
     app.delete('/wishlist/:productID', [AuthController.authenticateUser], UserController.deleteWishList);
-    //admin
-    app.post('/admin/product/upload-image', [AuthController.authenticateUser, AuthController.isAdmin, upload.single('image')], ImageUploadController.imageUploadSingleController);
-    app.post('/admin/brand/upload-image', [AuthController.authenticateUser, AuthController.isAdmin, upload.single('image')], ImageUploadController.imageUploadSingleController);
-    app.get('/admin/dashboard', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.getDashBoard);
-    app.get('/admin/users', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.getAllUsers);
-    app.get('/admin/orders', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.getAllOrders);
-    app.put('/admin/order/:orderID', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.updateOrderStatus);
-    app.get('/admin/products', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.getAllProducts);
-    app.post('/admin/product', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.addProduct);
-    app.put('/admin/product/:productID', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.updateProduct);
-    app.delete('/admin/product/:productID', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.deleteProduct);
-    app.get('/admin/brands', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.getAllBrands);
-    app.post('/admin/brand', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.addBrand);
-    app.put('/admin/brand/:brandID', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.updateBrand);
-    app.delete('/admin/brand/:brandID', [AuthController.authenticateUser, AuthController.isAdmin], AdminController.deleteBrand);
     return app.use("/", router);
 }
 
